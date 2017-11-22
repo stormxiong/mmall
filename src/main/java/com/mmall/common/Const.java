beead6f7bf3f6dccf2d96554982ada44b8a190e4
stormxiong <373250051@qq.com>
2017-11-22 13:34:14 +0800

    test

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -9,6 +9,8 @@ import java.util.Set;
  */
 public class Const {
 
+
+
     public static final String CURRENT_USER = "currentUser";
     public static final String EMAIL = "email";
     public static final String USERNAME = "username";
